2a808c1225334c357e28004cc767ae5728864126
sumitt06 <namdev27sumit@gmail.com>
2026-06-30 17:26:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.cpp
@@ -0,0 +1,26 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+}; */
+
+class Solution {
+  public:
+    bool searchKey(Node* head, int key) {
+        // Code here
+        int count = 0;
+        while(head != NULL) {
+            if(head -> data == key) {
+                return true;
+            }
+            head = head -> next;
+        }
+        return false;
+    }
+};

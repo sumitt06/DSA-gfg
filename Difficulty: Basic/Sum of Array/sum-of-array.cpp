6c40d94c6454e88c706ed5f4294f7192ddbfde4b
sumitt06 <namdev27sumit@gmail.com>
2026-09-08 17:08:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+  
+  int sum(vector<int>& arr, int n) {
+          if (n == 0) {
+              return 0;
+          }
+          return sum(arr, n - 1) + arr[n - 1];
+  }
+    int arraySum(vector<int>& arr) {
+        // code here
+        return sum(arr , arr.size());
+    }
+};
